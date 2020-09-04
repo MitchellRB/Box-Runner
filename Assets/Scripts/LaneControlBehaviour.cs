ae92d1620f47449478e569ff5cb6bd87fec4e7b9
MitchellRB <43767850+MitchellRB@users.noreply.github.com>
2020-09-04 10:17:24 +1000

    Added lane movement

---
+++ Assets/Scripts/LaneControlBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaneControlBehaviour : MonoBehaviour
+{
+    public float destroyThreshold = -30;
+    public float respawnDistance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.z < destroyThreshold)
+        {
+            Vector3 resPos = new Vector3(0, 0, respawnDistance);
+            transform.position = resPos;
+        }
+        else
+        {
+            transform.Translate(0, 0, -0.25f);
+        }
+    }
+}
---
+++ Assets/Scripts/LaneSpawnBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaneSpawnBehaviour : MonoBehaviour
+{
+    public GameObject laneController;
+    public float segmentLength;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        float spawnDistance = 0;
+
+        while (spawnDistance < transform.position.z + 30)
+        {
+            Instantiate(laneController, new Vector3(transform.position.x, transform.position.y, transform.position.z - spawnDistance), Quaternion.identity);
+            spawnDistance += segmentLength;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
