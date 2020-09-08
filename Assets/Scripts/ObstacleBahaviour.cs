2f77e38fe1c288dd46f989ec3a1046f87a7f9be1
MitchellRB <43767850+MitchellRB@users.noreply.github.com>
2020-09-09 09:48:56 +1000

    Added basic obstacle

---
+++ Assets/Scripts/ObstacleBahaviour.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleBahaviour : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public float movementSpeed;
+    public float destroyPosition;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.Translate(0, 0, movementSpeed * Time.fixedDeltaTime * -1);
+
+        if (transform.position.z < destroyPosition)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(PlayerController))]
+public class PlayerCollision : MonoBehaviour
+{
+    public string obstacleTag;
+
+    public Text deathText;
+
+    private PlayerController pc;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pc = gameObject.GetComponent<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag(obstacleTag))
+        {
+            deathText.gameObject.SetActive(true);
+            pc.enabled = false;
+        }
+    }
+}
